45ed31d7926e5f0acd2f5fc62f8dd055786ecab0
haotian wu <2371139W@student.gla.ac.uk>
2019-01-21 23:28:11 +0000

    first commit

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def about(request):
+    return HttpResponse("ssdsafdafdsa")
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +74,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
+
+
 
 
 # Database
